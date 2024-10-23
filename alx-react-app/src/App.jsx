1dd51e203ced96181d87b6c144b8cff39c90c7ba
RachieJosh <rachaeljosh23@gmail.com>
2024-10-23 17:22:48 +0100

    corrected an error

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import Header from "./components/Header"; // Import Header
 import MainContent from "./components/MainContent"; // Import MainContent
 import Footer from "./components/Footer"; // Import Footer
+import WelcomeMessage from "./components/WelcomeMessage";
 
 function App() {
   return (
     <div>
+      <WelcomeMessage />
       <Header /> {/* Include Header */}
       <MainContent /> {/* Include MainContent */}
       <Footer /> {/* Include Footer */}
