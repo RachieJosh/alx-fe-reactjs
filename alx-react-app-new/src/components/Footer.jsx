a9eba348673ba0b8efc4feab2766cf06e05162ec
RachieJosh <rachaeljosh23@gmail.com>
2024-10-23 18:16:47 +0100

    changed the files for a new project case

--- alx-react-app-new/src/components/Footer.jsx
+++ alx-react-app-new/src/components/Footer.jsx
@@ -1,9 +1,15 @@
-function Footer() {
+const Footer = () => {
   return (
-    <footer>
+    <footer
+      style={{
+        backgroundColor: "lightgray",
+        textAlign: "center",
+        padding: "10px",
+      }}
+    >
       <p>© 2023 City Lovers</p>
     </footer>
   );
-}
+};
 
 export default Footer;
