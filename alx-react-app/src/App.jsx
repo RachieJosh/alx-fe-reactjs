a55efbedf5e30a44ec38f2bcbd89de1c1e145755
RachieJosh <rachaeljosh23@gmail.com>
2024-10-23 17:27:05 +0100

    added a new file and completed project

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,6 +3,7 @@ import Header from "./components/Header"; // Import Header
 import MainContent from "./components/MainContent"; // Import MainContent
 import Footer from "./components/Footer"; // Import Footer
 import WelcomeMessage from "./components/WelcomeMessage";
+import UserProfile from "./components/UserProfile";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <WelcomeMessage />
       <Header /> {/* Include Header */}
       <MainContent /> {/* Include MainContent */}
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
       <Footer /> {/* Include Footer */}
     </div>
   );
