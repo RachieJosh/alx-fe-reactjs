fe3a09811ba45e2f0716e9e5c11d34a51e873b64
RachieJosh <rachaeljosh23@gmail.com>
2024-10-24 14:24:54 +0100

    corrected another error

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom"; // Import Routes and Route
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; // Import Router, Routes, and Route
 import RecipeList from "./components/RecipeList";
 import AddRecipeForm from "./components/AddRecipeForm";
 import RecipeDetails from "./components/RecipeDetails";
@@ -7,17 +7,22 @@ import EditRecipeForm from "./components/EditRecipeForm";
 
 const App = () => {
   return (
-    <Routes>
+    <Router>
       {" "}
-      {/* Use Routes to wrap your Route components */}
-      <Route path="/" element={<RecipeList />} /> {/* Main recipe list */}
-      <Route path="/add" element={<AddRecipeForm />} />{" "}
-      {/* Form to add a recipe */}
-      <Route path="/recipes/:id" element={<RecipeDetails />} />{" "}
-      {/* Recipe details page */}
-      <Route path="/edit/:id" element={<EditRecipeForm />} />{" "}
-      {/* Edit recipe page */}
-    </Routes>
+      {/* Wrap everything in Router */}
+      <div>
+        <h1>Recipe Sharing App</h1>
+        <Routes>
+          <Route path="/" element={<RecipeList />} /> {/* Home Page */}
+          <Route path="/add" element={<AddRecipeForm />} />{" "}
+          {/* Add Recipe Page */}
+          <Route path="/recipe/:id" element={<RecipeDetails />} />{" "}
+          {/* Recipe Details Page */}
+          <Route path="/edit/:id" element={<EditRecipeForm />} />{" "}
+          {/* Edit Recipe Page */}
+        </Routes>
+      </div>
+    </Router>
   );
 };
 
