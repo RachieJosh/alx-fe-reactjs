9bbfbb5a85957f5eef1ac89e8f7700d4151c8bac
RachieJosh <rachaeljosh23@gmail.com>
2024-10-24 19:58:20 +0100

    error corrected

--- recipe-sharing-platform/src/components/RecipeDetail.jsx
+++ recipe-sharing-platform/src/components/RecipeDetail.jsx
@@ -19,7 +19,7 @@ function RecipeDetail() {
   }
 
   return (
-    <div className="container mx-auto p-4">
+    <div className="container mx-auto p-4 shadow-lg rounded-lg bg-white">
       <h1 className="text-3xl font-bold mb-4">{recipe.title}</h1>
       <img
         src={recipe.image}
